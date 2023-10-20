06d1aaf1e2ce2c5995e0e7b14a8a9e82e954a7ca
Hugo Silva <145476600+Hugodzs@users.noreply.github.com>
2023-10-20 12:52:44 +0000

    s

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-let wins = 50      
-let loses = 25
 
 function rankingCalculator(wins, loses){
 
     return wins - loses
 } 
+let resultRanking = rankingCalculator(25,50)
 console.log(rankingCalculator)
