4cfd16f1b2a664d9ed0d48db1a8ac83ee4bb356d
Hugo Silva <145476600+Hugodzs@users.noreply.github.com>
2023-10-20 14:09:52 +0000

    ass

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 function rankingCalculator(wins, loses) {
     let result = wins - loses
 
-    console.log(result + " pontos! ")
-} 
-rankingCalculator(50,25);
+     switch(result){
+        case result <= 10: console.log("Heroi tem " + result + "pontos e está no ranking Ferro");
+        break;
+        case result >=10 && result <=20: console.log("Dia de começar a semana com prata!");
+        break;
+        default: console.log("erro")
+     }
+    }
+rankingCalculator(20,11);
+
